067d1183c374dfbb4aeb3f90278cf52102e6872a
Rehno Lindeque <errantkid@gmail.com>
2015-03-04 11:33:36 +0200

    Add zipExactN functions

--- Safe/Extra.hs
+++ Safe/Extra.hs
@@ -1,10 +1,13 @@
-module Safe.Extra 
+module Safe.Extra
   ( fromLeftNote'
   , fromRightNote'
   , fromRightPrefix'
   , zipWithExact3Note
   , zipWithExact4Note
   , zipWithExact5Note
+  , zipExact3Note
+  , zipExact4Note
+  , zipExact5Note
   ) where
 
 import Prelude
@@ -47,3 +50,12 @@ zipWithExact5Note note f as bs cs ds es =
   in  if minimum ls /= maximum ls
       then error $ "zipWith5 lists are different lengths " ++ show ls ++ ", " ++ note
       else zipWith5 f as bs cs ds es
+
+zipExact3Note :: String -> [a] -> [b] -> [c] -> [(a,b,c)]
+zipExact3Note note = zipWithExact3Note note (,,)
+
+zipExact4Note :: String -> [a] -> [b] -> [c] -> [d] -> [(a,b,c,d)]
+zipExact4Note note =  zipWithExact4Note note (,,,)
+
+zipExact5Note :: String -> [a] -> [b] -> [c] -> [d] -> [e] -> [(a,b,c,d,e)]
+zipExact5Note note =  zipWithExact5Note note (,,,,)

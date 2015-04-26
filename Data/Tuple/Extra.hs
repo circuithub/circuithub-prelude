e6a23552bc328796013445588fa22d71cf809245
Rehno Lindeque <errantkid@gmail.com>
2015-04-26 19:23:57 +0200

    Add uncurry alternative that works with nested pairs

--- Data/Tuple/Extra.hs
+++ Data/Tuple/Extra.hs
@@ -1,6 +1,7 @@
 {-# LANGUAGE TupleSections #-}
 module Data.Tuple.Extra where
 
+import Data.Tuple (uncurry)
 import Data.Functor
 
 uncurry3 :: (a -> b -> c -> d) -> (a, b, c) -> d
@@ -19,3 +20,18 @@ sequenceFst :: (Functor f) => (f a, b) -> f (a, b)
 sequenceFst (a,b) = fmap (,b) a
 sequenceSnd :: (Functor f) => (a, f b) -> f (a, b)
 sequenceSnd (a,b) = fmap (a,) b
+
+uncurryProduct :: (a -> b -> c) -> (a, b) -> c
+uncurryProduct = uncurry
+
+uncurryProduct3 :: (a -> b -> c -> d) -> (a, (b, c)) -> d
+uncurryProduct3 f (x,(y,z)) = f x y z
+
+uncurryProduct4 :: (a -> b -> c -> d -> e) -> (a, (b, (c, d))) -> e
+uncurryProduct4 f (w,(x,(y,z))) = f w x y z
+
+uncurryProduct5 :: (a -> b -> c -> d -> e -> f) -> (a, (b, (c, (d, e)))) -> f
+uncurryProduct5 f (v,(w,(x,(y,z)))) = f v w x y z
+
+uncurryProduct6 :: (a -> b -> c -> d -> e -> f -> g) -> (a, (b, (c, (d, (e, f))))) -> g
+uncurryProduct6 f (u,(v,(w,(x,(y,z))))) = f u v w x y z

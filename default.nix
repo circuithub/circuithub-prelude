4697782db12dc205e1b06e07d631d5bb7c5b3989
Oliver Charles <ollie@ocharles.org.uk>
2015-10-14 11:31:12 +0100

    nix: Add default.nix
    Automatically generated by cabal2nix

---
+++ default.nix
@@ -0,0 +1,19 @@
+{ mkDerivation, aeson, aeson-pretty, base, bifunctors, bytestring
+, classy-prelude, comparable-key, containers, either, errors
+, hashable, mono-traversable, safe, semigroups, split, stdenv, text
+, time, unordered-containers, utf8-string, vector
+}:
+mkDerivation {
+  pname = "circuithub-prelude";
+  version = "0.0.21";
+  src = ./.;
+  buildDepends = [
+    aeson aeson-pretty base bifunctors bytestring classy-prelude
+    comparable-key containers either errors hashable mono-traversable
+    safe semigroups split text time unordered-containers utf8-string
+    vector
+  ];
+  homepage = "https://github.com/circuithub/circuithub-prelude";
+  description = "CircuitHub's Prelude that builds on top of the ClassyPrelude";
+  license = stdenv.lib.licenses.mit;
+}

ac1f75f7f7784178743d7c5a501ab6ba0acb9078
Theunis <unohoo0@gmail.com>
2015-12-04 22:53:42 +0200

    Allow set of holiday dates to be excluded in addBusinessDays calculation

--- Data/Time/Extra.hs
+++ Data/Time/Extra.hs
@@ -1,33 +1,46 @@
 module Data.Time.Extra where
 
-import ClassyPrelude
+import ClassyPrelude hiding (member)
+import Data.Set
 import Data.Time
 import Data.Time.Calendar.WeekDate
 
 -- module takes monday as first day of the week
 
-addDaysInt :: Int -> Day -> Day 
-addDaysInt days = addDays (toInteger days)  
+addDaysInt :: Int -> Day -> Day
+addDaysInt days = addDays (toInteger days)
 
 -- adjusts the given day to the start of the following week
-toNextWeek :: Day -> Day 
+toNextWeek :: Day -> Day
 toNextWeek day = addDaysInt (7 - (dayOfWeek day) +1) day
 
-isWeekend :: Day -> Bool 
+isWeekend :: Day -> Bool
 isWeekend = (>5) . dayOfWeek
 
 dayOfWeek :: Day -> Int
-dayOfWeek day = 
-  let (_, _, dow) = toWeekDate day 
-  in dow 
+dayOfWeek day =
+  let (_, _, dow) = toWeekDate day
+  in dow
 
--- exclude weekends
--- todo take a list of holidays?
-addBusinessDays :: Int -> Day -> Day
-addBusinessDays days day = 
-  let day' = if isWeekend day then toNextWeek day else day 
+addBusinessDays' :: Int -> Day -> Day
+addBusinessDays' days day =
+  let day' = if isWeekend day then toNextWeek day else day
       dow  = dayOfWeek day'
       weeks = days `div` 5
       extraWeekendNeeded = (days `mod` 5) > (5 - dow)
       weekends = weeks + (if extraWeekendNeeded then 1 else 0 )
   in addDaysInt (days + 2 * weekends) day'
+
+-- | Simple iterative version which allows holidays to be specified
+addBusinessDays :: Set Day -- ^ Set of holidays
+                 -> Int     -- ^ Business days to add
+                 -> Day     -- ^ Starting day
+                 -> Day
+addBusinessDays holidays = go
+  where
+    isHoliday day = member day holidays
+    go days day
+      | isWeekend day || isHoliday day = go days (addDaysInt 1 day)
+      | days == 0                      = day
+      | otherwise                      = go (days - 1) (addDaysInt 1 day)
+
